cb9bae093cbf26b6c8c1d8676b4fd9414e59ffc8
kalyanakannan <kalyanakannan.p@gmail.com>
2015-11-27 23:07:30 +0530

    Create mysql-db.sh

---
+++ mysql-db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+MYSQL=`which mysql`
+
+Q1="CREATE DATABASE IF NOT EXISTS databasename;"
+Q2="CREATE USER username@localhost IDENTIFIED BY 'password';"
+Q3="GRANT ALL PRIVILEGES ON database.* TO username@localhost;"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+
+
+$MYSQL -uroot -p -e "$SQL"
+
+echo  "wordpress database created"
