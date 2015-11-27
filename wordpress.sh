2d9b07b5958510d3795eb5c5f1a109f56e9a7bcd
kalyanakannan <kalyanakannan.p@gmail.com>
2015-11-27 23:45:59 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -9,4 +9,6 @@ cp wp-config-sample.php wp-config.php
 sed -i 's/database_name_here/databasename/g' /home/webinar/wordpress/wp-config.php
 sed -i 's/username_here/username/g' /home/webinar/wordpress/wp-config.php
 sed -i 's/password_here/password/g' /home/webinar/wordpress/wp-config.php
-
+sudo rsync -avP ~/wordpress/ /var/www/html/
+mkdir /var/www/html/wp-content/uploads
+sudo chown -R :www-data /var/www/html/wp-content/uploads
