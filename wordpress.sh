210423ff211501fb6ad1b405e1e6ff04d51d5978
kalyanakannan <kalyanakannan.p@gmail.com>
2015-11-27 23:41:49 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -6,3 +6,7 @@ sudo apt-get update
 sudo apt-get install php5-gd libssh2-php
 cd ~/wordpress
 cp wp-config-sample.php wp-config.php
+sed -i 's/database_name_here/databasename/g' /home/webinar/wordpress/wp-config.php
+sed -i 's/username_here/username/g' /home/webinar/wordpress/wp-config.php
+sed -i 's/password_here/password/g' /home/webinar/wordpress/wp-config.php
+
