9a62282442c57ece629ccafe282e39c1076de45b
kalyanakannan <kalyanakannan.p@gmail.com>
2015-12-15 12:28:34 +0530

    Update mysql-db.sh

--- mysql-db.sh
+++ mysql-db.sh
@@ -6,7 +6,7 @@ MYSQL=`which mysql`
 
 Q1="CREATE DATABASE IF NOT EXISTS databasename;"
 Q2="CREATE USER username@localhost IDENTIFIED BY 'password';"
-Q3="GRANT ALL PRIVILEGES ON database.* TO username@localhost;"
+Q3="GRANT ALL PRIVILEGES ON databasename.* TO username@localhost;"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
 
